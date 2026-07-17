ada39e5fd920e40189dfaa5fe66bc45e30394bab
Kosta Gerodimos <kosta@gerodimos.com>
2026-07-17 12:53:24 +0200

    Add development server entrypoint

--- mnemosyne.py
+++ mnemosyne.py
@@ -158,3 +158,9 @@ def main() -> None:
     import uvicorn
 
     uvicorn.run("mnemosyne:app", host="127.0.0.1", port=8000)
+
+
+def dev() -> None:
+    import uvicorn
+
+    uvicorn.run("mnemosyne:app", host="127.0.0.1", port=8000, reload=True)
